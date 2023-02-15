41085d6a71f1874324694d1f8be982d863f902e0
Muhammad Atif <muhammadatifkhan.99@gmail.com>
2023-02-15 18:41:22 +0500

    calculate the result of expression

---
+++ calculatingtheresultoftheexpression.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main (){
+	
+	//a = 10, b= 20, c=15, d= 8, e= 40;
+	//a*b(-c*31%13)d
+	int a, b ,c ,d,e;
+	int result{0};
+	a = 10, b= 20, c=15, d= 8, e= 40;
+	
+	cout<<a*b/(-c*31%13)*MessageBox(,"Hello","Caption",MB_OK);
+d<<" is the result for the expression"<<endl;
+	
+	return 0;
+}
