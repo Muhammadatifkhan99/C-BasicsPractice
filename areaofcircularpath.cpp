0f87324876265cfb5195a9677d13f97af7ffa807
Muhammad Atif <muhammadatifkhan.99@gmail.com>
2023-02-15 18:51:48 +0500

    calculate area of circul

---
+++ areaofcircularpath.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<conio.h>
+#define PI 3.1458
+
+using namespace std;
+
+int main (){
+    cout<<"Enter value of radius: ";
+    int r;
+    cin>>r;
+    int area;
+    area = 2 * PI *r;
+    cout<<area<<endl;
+}
