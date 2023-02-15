6f7e62ca7d9fa662cc1d78dcd759e57b1ac79fc1
Muhammad Atif <muhammadatifkhan.99@gmail.com>
2023-02-15 18:49:35 +0500

    Compound assignments

---
+++ ITSeriesBookCompoundAssignment.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+
+using namespace std;
+
+
+int main(){
+    int a;
+    a = 5;
+    cout<<"Value fo a =:"<<a<<endl;
+    a += 5;
+    cout<<"value for a += 10:"<<a<<endl;
+    a -=2;
+    cout<<"value for a -=10:"<<a<<endl;
+    a *=2;
+    cout<<"value for a *=10:"<<a<<endl;
+    a /=2;
+    cout<<"value for a /=10:"<<a<<endl;
+    a %=2;
+    cout<<"value for a %=10:"<<a<<endl;
+    
+    return 0;
+}
